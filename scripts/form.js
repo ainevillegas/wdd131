006fc0d16e6065043b4c90f4fa3d00e4aa917f1d
ainesider <lcurrovillegas@byupathway.edu>
2026-05-25 10:37:58 +0800

    commit

--- scripts/form.js
+++ scripts/form.js
@@ -6,14 +6,11 @@ const products = [
     { id: "p4", name: "Smartwatch" }
 ];
 
-// ✅ Populate select
-const productSelect = document.getElementById("product");
+const select = document.getElementById("product");
 
 products.forEach(product => {
     const option = document.createElement("option");
-
-    option.value = product.id;   // value = id
-    option.textContent = product.name; // display = name
-
-    productSelect.appendChild(option);
+    option.value = product.id;
+    option.textContent = product.name;
+    select.appendChild(option);
 });
